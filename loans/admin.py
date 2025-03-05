2c652eb6034cfdceb3ae78f646da53940a632629
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-03-05 15:55:18 +0200

    Write model admin classes

--- loans/admin.py
+++ loans/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import LoanFundType, LoanFund, LoanType, Loan, AmortizationSchedule
+
+
+class AmortizationScheduleInlineAdmin(admin.TabularInline):
+    model = AmortizationSchedule
+    min_num = 0
+    extra = 1
+    readonly_fields = ('create_at', 'update_at')
+
+
+@admin.register(LoanFundType)
+class LoanFundTypeModelAdmin(admin.ModelAdmin):
+    list_display = ['personnel', 'name', 'create_at', 'update_at']
+    readonly_fields = ('create_at', 'update_at')
+
+
+@admin.register(LoanFund)
+class LoanFundModelAdmin(admin.ModelAdmin):
+    list_display = ['provider', 'loan_type', 'amount', 'create_at', 'update_at']
+    readonly_fields = ('create_at', 'update_at')
+
+
+@admin.register(LoanType)
+class LoanTypeModelAdmin(admin.ModelAdmin):
+    list_display = ['personnel', 'name', 'create_at', 'update_at']
+    readonly_fields = ('create_at', 'update_at')
+
+
+@admin.register(Loan)
+class LoanModelAdmin(admin.ModelAdmin):
+    list_display = ['customer', 'loan_type', 'status', 'amount', 'create_at', 'update_at']
+    readonly_fields = ('create_at', 'update_at')
+    inlines = [AmortizationScheduleInlineAdmin]
