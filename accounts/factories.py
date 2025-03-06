b7c440de6f62ef2d2a79b04a06e46e881bc85e50
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-03-06 23:45:09 +0200

    Write user factory

---
+++ accounts/factories.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+import factory
+
+from accounts.models import User
+from accounts.enums import UserRole
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    email = factory.LazyAttribute(lambda obj: f'{obj.username}@example.com')
+    first_name = factory.Faker('first_name')
+    last_name = factory.Faker('last_name')
+    is_active = True
+    role = factory.Iterator([role.value for role in UserRole if role != UserRole.ADMIN])
+    password = factory.PostGenerationMethodCall('set_password', 'defaultpassword')
+
+    @factory.lazy_attribute_sequence
+    def username(self, n):
+        max_pk = User.objects.aggregate(max_pk=models.Max('pk'))['max_pk'] or 0
+        return f'user_{max(max_pk, n) + 1}'
+
+    class Meta:
+        model = User
